4ead57bc288e5bc90cb123bda8bbd89e3f132dcb
leahannejohnson <lajohnson22@amherst.edu>
2022-01-30 17:12:24 -0500

    add spam data with features

---
+++ data-raw/spam_features_load.R
@@ -0,0 +1,33 @@
+library(textclassificationexamples)
+library(tidyverse)
+
+spam_train <- emails_train |>
+  na.omit() |>
+  mutate(
+    all_caps = as.factor(all_caps(subjectline)), 
+    has_dollar_sign = as.factor(has_dollar_sign(subjectline)), 
+    has_ampersand = as.factor(has_dollar_sign(subjectline)), 
+    has_dear = as.factor(has_dear(subjectline)), 
+    has_mister = as.factor(has_mister(subjectline)), 
+    ends_with_period = as.factor(ends_with_period(subjectline)), 
+    multiple_punctuation = as.factor(multiple_punctuation(subjectline)), 
+    has_religious = as.factor(has_religious(subjectline)), 
+    begins_re = as.factor(begins_re(subjectline))
+  ) 
+
+spam_test <- emails_test |>
+  na.omit() |>
+  mutate(
+    all_caps = as.factor(all_caps(subjectline)), 
+    has_dollar_sign = as.factor(has_dollar_sign(subjectline)), 
+    has_ampersand = as.factor(has_dollar_sign(subjectline)), 
+    has_dear = as.factor(has_dear(subjectline)), 
+    has_mister = as.factor(has_mister(subjectline)), 
+    ends_with_period = as.factor(ends_with_period(subjectline)), 
+    multiple_punctuation = as.factor(multiple_punctuation(subjectline)), 
+    has_religious = as.factor(has_religious(subjectline)), 
+    begins_re = as.factor(begins_re(subjectline))
+  ) 
+
+usethis::use_data(spam_train, overwrite = TRUE)
+usethis::use_data(spam_test, overwrite = TRUE)
